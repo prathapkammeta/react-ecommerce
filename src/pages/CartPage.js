84f782897065389b1714a94d59cd5af4d85984c7
prathapkammeta905 <prathapkammeta905@gmail.com>
2023-06-27 18:37:37 +0530

    first one hour 13 mints

---
+++ src/pages/CartPage.js
@@ -0,0 +1,13 @@
+import Cart from '../features/cart/Cart'
+import React from 'react'
+
+function CartPage() {
+  return (
+
+    <div>
+        <Cart></Cart>
+    </div>
+    )
+}
+
+export default CartPage
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../features/navbar/NavBar'
+import ProductList from '../features/product-list/components/ProductList'
+function Home() {
+  return (
+
+            <div>
+               <NavBar>
+               <ProductList></ProductList>
+               </NavBar>  
+            </div>
+    )
+}
+
+export default Home
